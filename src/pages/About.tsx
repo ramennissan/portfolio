bd92e32d39a27eea2b5a4de1bd03610815518855
Ramen Nissan <ramen.nissan@hotmail.com>
2026-01-06 08:58:26 -0500

    feat: restore GitHubChart component to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,13 @@
+import GitHubChart from '../components/GitHubChart';
+
 export default function About() {
   return (
     <section style={{ padding: '2rem' }}>
       <h1>About Me</h1>
       <p></p>
+      <div>
+        <GitHubChart username="RamenNissan" />
+      </div>
     </section>
   )
 }
