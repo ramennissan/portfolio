15f55c91a47d6e0139d478175c9011b2f3c81158
ramennissan <ramennissan@users.noreply.github.com>
2026-04-26 20:56:10 +0000

    deploy: ae30c5f213b300ca8fae2ae29299e4d7a329dd49

---
+++ assets/researchData.js
@@ -0,0 +1,1 @@
+const e=[{id:"research-1",title:"LLM-Guided Dexterous Manipulation via Reinforcement Learning",description:"Currently working on a paper that utilizes Reinforcement Learning (RL) and Large Language Models (LLMs) to enhance robotic hand dexterity and grasping.",image:"/assets/GIFS/research%20demo.gif",tags:["LLM","Deep RL","PyBullet"],paperLink:"#"}];export{e as s};
